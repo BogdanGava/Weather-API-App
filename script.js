94b5477539712a29aca2163e07b4e1e00b601806
Bogdan Gava <63742326+BogdanGava@users.noreply.github.com>
2021-08-08 15:00:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,14 +9,14 @@ button.addEventListener('click', function(){
   fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=2ee8efd8b079767a62d68b802236486a')
   .then(response => response.json())
   .then(data => {
-    let tempValue = data.main.temp;
+    let tempValue = Math.round(data.main.temp-273);
     let nameValue = data.name;
     let descValue = data.weather[0].description;
     let icon = data.weather[0].icon
 
     main.innerHTML = nameValue;
     desc.innerHTML = "Description:  "+descValue;
-    temp.innerHTML = "Temperature:  "+tempValue;
+    temp.innerHTML = "Temperature:  "+tempValue+" °C";
     locationIcon.innerHTML = `<img src="icons/${icon}.png">`
     input.value ="";
   
@@ -30,14 +30,14 @@ button.addEventListener('click', function(){
     {fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=2ee8efd8b079767a62d68b802236486a')
     .then(response => response.json())
     .then(data => {
-      let tempValue = data.main.temp;
+      let tempValue = Math.round(data.main.temp-273);
       let nameValue = data.name;
       let descValue = data.weather[0].description;
       let icon = data.weather[0].icon
 
       main.innerHTML = nameValue;
       desc.innerHTML = "Description:  "+descValue;
-      temp.innerHTML = "Temperature:  "+tempValue;
+      temp.innerHTML = "Temperature:  "+tempValue+" °C";
       locationIcon.innerHTML = `<img src="icons/${icon}.png">`
       input.value ="";
      
